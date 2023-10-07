766d91d7c63b068e458232044f0aa7574dc3da5f
raistmere <raistmere@gmail.com>
2023-10-07 18:27:12 -0500

    Finished testing gameboard.js with jest
    Currently finished majority of the gameboard.js test using jest. Moving
    onto the player.js for testing.

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,6 +1,12 @@
 const gameboard = () => {
+    // This variable is the main board that holds all the data for all the cells
     let board = [];
+    //  This variable holds all the active ships on the board (For this specific board).
     let activeShips = [];
+    // This variable holds all the miss attempts positions on the board (x, y)
+    let missPosList = [];
+    // This variable holds all the hit attempts positions on the board (x, y)
+    let hitPosList = [];
 
     const initilization = () =>
     {
@@ -14,7 +20,10 @@ const gameboard = () => {
                 board[i].push
                 (
                     {
-                        cellState: -1,
+                        //State keeps track if the cell has been attacked
+                        // 0 = nothing, -1 = miss, 1 = hit
+                        state: 0,
+                        // This holds the reference to a ship object
                         shipRef: null,
                     }
                 );          
@@ -27,31 +36,72 @@ const gameboard = () => {
     };
 
     const canWeAddShip = (ship, x, y, direction) => {
-        switch (direction) 
+        // Check to see what direction the rear of the boat is heading based on (x, y) front position.
+        if(direction === "down")
         {
-            case "up":
-                for (let i = y; i > 0; i--) 
-                {
-                    if(board[i][x].shipRef !== null) { return false; }
-                }
+            // Check to see if the cells we need is empty or avaliable for ship placement.
+            for (let i = 0; i < ship.getLength(); i++) 
+            {
+                // Check the edge case where the ship might go out of bounds of the array.
+                if((y + 1) > 10) { return false; }
 
-                activeShips.push(ship);
-                for (let i = y; i > 0; i--) 
+                // Check to see if there are any other ships already on the board cells.
+                if(board[(y + i)][x].shipRef !== null)
                 {
-                    addShip(ship, x, y);
-                    console.log(board[i][x].shipRef);
+                    // This return false allows us to tell whoever called the method that we are "NOT GOOD TO GO *Thumbs down*".
+                    return false; 
                 }
+            }
 
-                return true;
+            // At this point, the cells are empty and we can go ahead and add the ship to the board.
+            // Add ship to activeShips array
+            activeShips.push(ship);
+            // Add ship references to the board cells based on the (x, y) + ship length offset.
+            for (let i = 0; i < ship.getLength(); i++) 
+            {
+                board[(y + i)][x].shipRef = ship;
+            }
+
+            // This return true allows us to tell whoever called the method that we are "GOOD TO GO! *Thumbs up*".
+            return true;
         }
-
-        return false;
     };
 
-    const addShip = (ship, x, y) => {
+    // This method handles receiving an attack from the current player.
+    const getAttack = (x, y) => {
+        // Get the cell data
+        let cell = getBoardCell(x, y);
+        // Check to see if the cell has a ship on it
+        if(cell.shipRef)
+        {
+            // Because there is a ship, we go ahead and declare a hit. (1 = hit for the state)
+            cell.state = 1;
+            // Because there is a ship, we want to make sure to call the hit() for that ship.
+            cell.shipRef.hit();
+            // Make sure to add to the hitPosList to keep track of all the hits we made
+            hitPosList.push({x, y});
+            // Return true that we hit a ship
+            return true;
+        }
+        // If there is no ship on the cell, we want to declare a miss.
+        else
+        {
+            // Because there is no ship, we go ahead and declare a miss (-1 = miss)
+            cell.state = -1;
+            // Make sure to add the miss position to the missPosList for tracking misses
+            missPosList.push({x, y});
+            // Return false that we missed our attack
+            return false;
+        }
+    }
 
-        board[y][x].shipRef = ship;
-    };
+    const getMissCount = () => {
+        return missPosList.length;
+    }
+
+    const getHitCount = () => {
+        return hitPosList.length;
+    }
 
     const getBoardCell = (x, y) => {
         return board[y][x];
@@ -71,7 +121,9 @@ const gameboard = () => {
     return {
         getActiveShips,
         canWeAddShip,
-        addShip,
+        getAttack,
+        getMissCount,
+        getHitCount,
         getBoardCell,
         getTotalCells,
     };
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -5,27 +5,47 @@ const ship = require("./ship.js")
 test("Testing gameboard functions", () => {
     let gameboard1 = gameboard();
     let newShip = ship(3);
-    let newShip2 = ship(5);
-
-   
 
     // TEST: Want to make sure we always have the correct amount of cells on the board. Should be 100 cells (10x10).
     expect(gameboard1.getTotalCells()).toBe(100);
     // TEST: Check to see if we can place the new ship on the board. Should return true or false.
     // If the expect is false, that means that the ship can not be added due to placement error. This means that we have to pick different placement.
     // True means that we can place it on the board.
-    expect(gameboard1.canWeAddShip(newShip, 1, 1, "up")).toBe(true);
+    expect(gameboard1.canWeAddShip(newShip, 0, 0, "down")).toBe(true);
+    // TEST: We want to check to see when we CANNOT place a ship on the board.
+    // We need to check for edge cases in the board array. board[0][11] is not possible.
+    expect(gameboard1.canWeAddShip(newShip, 0, 10, "down")).toBe(false);
     // TEST: Check to see if adding a new ship works.
     expect(gameboard1.getActiveShips()).toBe(1);
-    // TEST: Check to see if the board cell has the correct shipRef
-    expect(gameboard1.getBoardCell(1, 1).shipRef).toStrictEqual(newShip);
-    // TEST: Check to see if the board cell has the correct shipREF length (Deeper check)
-    expect(gameboard1.getBoardCell(1, 1).shipRef.getLength()).toBe(newShip.getLength());
+    // TEST: Check to see if the board cells has the correct shipRef
+    // In this case, newShip has 3 length, therefore 3 board cells should have the same shipRef
+    // We added the newShip to the board with an down direction. So we check cells down 2 from (x, y) coords
+    expect(gameboard1.getBoardCell(0, 0).shipRef).toStrictEqual(newShip);
+    expect(gameboard1.getBoardCell(0, 1).shipRef).toStrictEqual(newShip);
+    expect(gameboard1.getBoardCell(0, 2).shipRef).toStrictEqual(newShip);
+    // TEST: Check to see if the same board cells has the correct shipREF length (Deeper check)
+    expect(gameboard1.getBoardCell(0, 0).shipRef.getLength()).toBe(newShip.getLength());
+    expect(gameboard1.getBoardCell(0, 1).shipRef.getLength()).toBe(newShip.getLength());
+    expect(gameboard1.getBoardCell(0, 2).shipRef.getLength()).toBe(newShip.getLength());
     // TEST: Checking to see if the newShip on the board is passed by reference not value.
-    // Calling newShip.hit() and check to see if it reflects on the board.
+    // Calling newShip.hit() and check to see if it reflects on the same board cells.
     newShip.hit();
     expect(newShip.checkHitCount()).toBe(1); // Should pass because we just called it.
-    expect(gameboard1.getBoardCell(1, 1).shipRef.checkHitCount()).toBe(1); // If it's passed by ref, then this should also be 1
-
+    expect(gameboard1.getBoardCell(0, 0).shipRef.checkHitCount()).toBe(1); // If it's passed by ref, then this should also be 1
+    expect(gameboard1.getBoardCell(0, 1).shipRef.checkHitCount()).toBe(1); // If it's passed by ref, then this should also be 1
+    expect(gameboard1.getBoardCell(0, 2).shipRef.checkHitCount()).toBe(1); // If it's passed by ref, then this should also be 1
+    // TEST: Check to see if the board can receive an attack command from the player
+    // true = ship exist and can be attack, false = ship does not exist
+    // Reason: We might need a return value for attacking to give the player a visual feedback.
+    expect(gameboard1.getAttack(0, 0)).toBe(true); // There is a ship at (x, y), so this should return true
+    expect(gameboard1.getAttack(1, 0)).toBe(false); // There is no ship at (x, y), so this should return false
+    // TEST: Check to see if the board cell at (0, 0) did get hit and check state
+    // Check to see if the newShip did get attacked at (0, 0)
+    expect(gameboard1.getBoardCell(0, 0).state).toBe(1); // There is a ship on the cell so it will be a hit.
+    expect(gameboard1.getBoardCell(0, 0).shipRef.checkHitCount()).toBe(2); // There was a hit, so the ship should have 2 hits now (1 from previous test)
+    expect(gameboard1.getBoardCell(1, 0).state).toBe(-1); // This checks an empty cell for a miss test
+    // TEST: Check to see if we can keep track of all the misses (totalMissCount, missPosList)
+    expect(gameboard1.getMissCount()).toBe(1); // This should be 1 beacause of the above miss test.
+    // TEST:Check to see if we can keep track of all the hit positions on the board
+    expect(gameboard1.getHitCount()).toBe(1); // Hit count for this should be 1 because we only did one getAttack() call. The newShip.hit() is not a getAttack() call.
 });
-
