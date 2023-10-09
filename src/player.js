1efaafff5257c2e4570c55ad8df4d29654478865
raistmere <raistmere@gmail.com>
2023-10-09 13:52:51 -0500

    Added player.js + jest testing for file
    Added the player.js and jest testing for the player.js file. Player.js
    has the core code functionality and possible addition might be necessary.

---
+++ src/player.js
@@ -0,0 +1,35 @@
+const player = () => {
+    // Variable keeps track of all the attack calls we have made.
+    const attackCallList = [];
+
+    // This method handles the player's attack call on (x, y)
+    const attack = (x, y) => {
+        // If (x, y) has been attacked already, then we return false because we cannot attack twice
+        if(checkAttack(x, y)){ return false };
+        // We can attack (x, y)
+        // Call pubsub event that we are attacking (x, y)
+
+        // Make sure to add the attack call coords to the attackCallList for future checks
+        attackCallList.push({x, y});
+        return true;
+    };
+
+    // This method handles checking to see if we already made an attack call at (x, y)
+    const checkAttack = (x, y) => {
+        let coord = {x, y};
+        // I tried multiple different array methods and for some reason only this one works in this format.
+        // I tried doing if(element === coord) but it never works. Doesn't do a deep check I guess?
+        return attackCallList.some((element) => {
+            if(element.x === coord.x && element.y === coord.y)
+            {
+                return true;
+            }
+        });
+    };
+
+    return {
+        attack,
+    };
+}
+
+module.exports = player;
---
+++ src/player.test.js
@@ -0,0 +1,11 @@
+const player = require("./player.js");
+
+test("Can player attack?", () => {
+    let player1 = player();
+
+    // Check to see if the player can attack that cell
+    expect(player1.attack(0, 0)).toBe(true);
+    // Test to make sure that if the cell cannot be attacked
+    expect(player1.attack(0, 0)).toBe(false);
+})
+
